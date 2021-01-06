728f7680771e2cb0b29f193a09b74739a5c5a83c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-06 18:04:57 -0100

    Implemented auto-scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,34 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //Options
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true }) //The location.search property contains the query string of an URI (including the ?) , we parse it and save the room and the username
-console.log(username)
+
+
+
+const autoScroll = () => {
+    //New message element
+    const $newMessage = $messages.lastElementChild
+
+    //Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+
+    //Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    //How far have I scrolled
+    const scrollOffSet = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffSet) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+
+
 
 socket.on('message', (message) => {
     console.log(message.text)
@@ -29,7 +56,7 @@ socket.on('message', (message) => {
     }) 
     
     $messages.insertAdjacentHTML('beforeend', html)
-    
+    autoScroll()
 })
 
 
@@ -43,6 +70,7 @@ socket.on('locationMessage', (message) => {
     })
     console.log(html)
     $messages.insertAdjacentHTML('beforeend', html)
+    autoScroll()
     
 })
 
